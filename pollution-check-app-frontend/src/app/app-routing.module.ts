f2d8a087c6373843032bb5c66c6f08835c0a133c
747616 <aaquibazhar1802@gmail.com>
2022-06-03 15:25:31 +0530

    Added home component, background images, removed register path, changed some routes, edited navbar and changed theme to indigo pink

---
+++ pollution-check-app-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './Components/home/home.component'
+import { LoginComponent } from './Components/login/login.component'
+import { RegisterComponent } from './Components/register/register.component'
+import { SearchAqiComponent } from './Components/search-aqi/search-aqi.component'
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'accounts/login', component: LoginComponent },
+  { path: 'search', component: SearchAqiComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ pollution-check-app-frontend/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ToolbarComponent } from './Components/toolbar/toolbar.component'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { LoginComponent } from './Components/login/login.component'
+import { RegisterComponent } from './Components/register/register.component'
+import { NgMaterialModule } from './Modules/ng-material/ng-material.module'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HomeComponent } from './Components/home/home.component'
+import { HttpClientModule } from '@angular/common/http'
+import { SearchAqiComponent } from './Components/search-aqi/search-aqi.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    SearchAqiComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    NgMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
